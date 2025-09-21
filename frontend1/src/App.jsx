a48cc2d267ffaae63280e1c606aec2beb6aac03a
VENKATARAJU <161044578+venkataraju0707@users.noreply.github.com>
2025-09-21 18:14:05 +0530

    Update App.jsx

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { MdEmail, MdPhone, MdStar, MdStarBorder, MdEdit, MdDelete } from "react-icons/md";
 
-const API_URL = "process.env.REACT_APP_API_URL;";
+const API_URL =import.meta.env.VITE_API_URL;
 
 function App() {
   const [contacts, setContacts] = useState([]);
